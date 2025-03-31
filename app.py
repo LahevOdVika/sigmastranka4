a3ce9086ec8def40a99805cca5acb85c73ba522d
MrWalper <MrWalper@proton.me>
2025-03-31 19:24:11 +0200

    Přidaný base.html renderer

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return render_template('index.html')
 
+@app.route('/base')
+def base():
+    return render_template('base.html') # Render na base, hlavně na debug
+
 
 if __name__ == '__main__':
     app.run(debug=True)
