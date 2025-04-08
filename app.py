996483244b8b02c38e2a61f3d644d1a86a2344de
LahevOdVika <michalBudai@outlook.cz>
2025-04-08 20:33:42 +0200

    Added form

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def kontakt():
     if request.method == 'POST':
         with databaseHandler() as db:
             db.addUser(request.form['name'], request.form['email'], request.form['phoneNumber'], request.form['message'])
-    return render_template('kontakt.html') 
+    return render_template('kontakt/kontakt.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
