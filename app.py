a825f7206792651bf93ef14416ae2664c1ccb9f6
LahevOdVika <michalBudai@outlook.cz>
2025-04-11 21:56:38 +0200

    Working on post something shit

--- app.py
+++ app.py
@@ -3,26 +3,39 @@ from dbHandler import databaseHandler
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     return render_template('home/index.html')
 
+
 @app.route('/base')
 def base():
-    return render_template('base.html') # Render na base, hlavně na debug
+    return render_template('base.html')  # Render na base, hlavně na debug
+
 
 @app.route('/kontakt', methods=['POST', 'GET'])
 def kontakt():
     if request.method == 'POST':
         with databaseHandler() as db:
-            db.addUser(request.form['name'], request.form['email'], request.form['phoneNumber'], request.form['message'])
+            db.addUser(request.form['name'], request.form['email'], request.form['phoneNumber'],
+                       request.form['message'])
     return render_template('kontakt/kontakt.html')
 
-@app.route('/iphone')
+
+@app.route('/iphone', methods=['POST', 'GET'])
 def iphone():
     with databaseHandler() as db:
-        print(db.getPhoneModels())
         return render_template('order/order.html', phone_models=db.getPhoneModels())
+
+
+@app.route('/design', methods=['POST', 'GET'])
+def design(model):
+    if request.method == 'POST':
+        model = request.get_json()['model']
+    print(model)
+    return render_template('designer/designer.html', model=model)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/order/order.js
+++ static/order/order.js
@@ -1,5 +1,18 @@
 function redirectToDesignPage(model) {
-    model = model.toLowerCase();
-    model = model.replaceAll(/\s/g, '');
-    window.location.href = 'design/' + model + '/';
+    fetch('/design', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            model: model
+        })
+    })
+    .then(response => response.text())
+    .then(data => {
+        window.location.href = '/design';
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
 }
